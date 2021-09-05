ea973f3964accf2e938b433a9b00ea14d78acfe0
jesaguilarhub <aguilar.codv@gmail.com>
2021-09-05 17:59:43 -0500

    Resolviendo ejercicio

--- services/db.services.js
+++ services/db.services.js
@@ -1,65 +1,88 @@
-import fs from "fs/promises";
-import path from "path";
-import faker from "faker";
+import fs from 'fs/promises';
+import path from 'path';
+import faker from 'faker';
 
 class AcademloDb {
+	static dbPath = path.resolve('db', 'db.json');
 
-    static dbPath = path.resolve("db", "db.json");
+	static findAll = async () => {
+		try {
+			let data = await fs.readFile(this.dbPath, 'utf8');
+			return JSON.parse(data);
+		} catch (error) {
+			throw new Error('Hubo un error al tratar de obtener todos los registros de la DB');
+		}
+	};
 
-    static findAll = async() => {
-        try{
-            let data = await fs.readFile(this.dbPath, "utf8");
-            return JSON.parse(data);
-        }catch(error){
-            throw new Error("Hubo un error al tratar de obtener todos los registros de la DB");
-        }
-    }
+	static writeAll = async (dbPath, data) => {
+		try {
+			await fs.writeFile(dbPath, data);
+		} catch (e) {
+			throw e;
+		}
+	};
+	static findById = async (id) => {
+		const users = await this.findAll();
+		return users.find((user) => user.id === id);
+	};
 
-    static findById = id => {
-    
-    }
+	static create = async (obj) => {
+		const users = await this.findAll();
+		users.push(obj);
+		await this.writeAll(this.dbPath, JSON.stringify(users));
+		return users.pop();
+	};
 
-    static create = (obj, id) => {
+	static update = async (obj, id) => {
+		const users = await this.findAll();
+		const idx = users.findIndex((user) => user.id === id);
 
-    }
+		if (idx < 0) throw new Error();
 
-    static update = (obj, id) => {
+		users[idx] = { id, ...obj };
+		await this.writeAll(this.dbPath, JSON.stringify(users));
+		return users[idx];
+	};
 
-    }
+	static delete = async (id) => {
+		const users = await this.findAll();
+		if (users.findIndex((user) => user.id === id) < 0) return false;
 
-    static delete = id => {
+		const updated = users.filter((user) => user.id !== id);
 
-    }
+		await this.writeAll(this.dbPath, JSON.stringify(updated));
 
-    static clear = async() => {
-        try{
-            await fs.writeFile(this.dbPath, JSON.stringify([]));
-        }catch(error){
-            throw new Error("Hubo un error al tratar de vaciar la DB");
-        }
-    }
+		return true;
+	};
 
-    static populateDB = async(size) => {
-        let userArr = [];
-        for(let i = 0; i<size; i++){
-            let userObj = {
-                id: i + 1,
-                firstname: faker.name.firstName(),
-                lastname: faker.name.lastName(),
-                email: faker.internet.email().toLowerCase()
-            };
+	static clear = async () => {
+		try {
+			await fs.writeFile(this.dbPath, JSON.stringify([]));
+		} catch (error) {
+			throw new Error('Hubo un error al tratar de vaciar la DB');
+		}
+	};
 
-            userArr.push(userObj);
-        }
+	static populateDB = async (size) => {
+		let userArr = [];
+		for (let i = 0; i < size; i++) {
+			let userObj = {
+				id        : i + 1,
+				firstname : faker.name.firstName(),
+				lastname  : faker.name.lastName(),
+				email     : faker.internet.email().toLowerCase()
+			};
 
-        try{
-            await fs.writeFile(this.dbPath, JSON.stringify(userArr));
-            return userArr;
-        }catch(error){
-            throw new Error("Hubo un error al insertar en la base de datos");
-        }
-    }
+			userArr.push(userObj);
+		}
 
+		try {
+			await fs.writeFile(this.dbPath, JSON.stringify(userArr));
+			return userArr;
+		} catch (error) {
+			throw new Error('Hubo un error al insertar en la base de datos');
+		}
+	};
 }
 
 export default AcademloDb;
--- tests/service.test.js
+++ tests/service.test.js
@@ -1,146 +1,173 @@
-import db from "../services/db.services.js";
-import faker from "faker";
+import db from '../services/db.services.js';
+import faker from 'faker';
 
-describe("Servicio findAll de DB", () => {
-    beforeAll(async () => {
-        await db.clear(); //Limpiar la DB
-        await db.populateDB(10); //Agregar 10 registros en la DB
-    });
+describe('Servicio findAll de DB', () => {
+	beforeAll(async () => {
+		await db.clear(); //Limpiar la DB
+		await db.populateDB(10); //Agregar 10 registros en la DB
+	});
 
-    it("Debería obtener un arreglo", async () => {
-        const users = await db.findAll();
+	it('Debería obtener un arreglo', async () => {
+		const users = await db.findAll();
 
-        expect(users).toEqual(expect.any(Array));
-    });
+		expect(users).toEqual(expect.any(Array));
+	});
 
-    it("Debería de tener 10 elementos en el arreglo", async () => {
-        const users = await db.findAll();
+	it('Debería de tener 10 elementos en el arreglo', async () => {
+		const users = await db.findAll();
 
-        expect(users.length).toBe(10);
-    });
+		expect(users.length).toBe(10);
+	});
 });
 
-describe("Servicio findById", () => {
-    let users = [];
+describe('Servicio findById', () => {
+	let users = [];
 
-    beforeAll(async () => {
-        await db.clear(); //Limpiar la DB
-        users = await db.populateDB(10); //Agregar 10 registros en la DB
-    });
+	beforeAll(async () => {
+		await db.clear(); //Limpiar la DB
+		users = await db.populateDB(10); //Agregar 10 registros en la DB
+	});
 
-    it("Debería de obtener un objeto", async () => {
-        const usersObj = await db.findById(1);
+	it('Debería de obtener un objeto', async () => {
+		const usersObj = await db.findById(1);
 
-        expect(usersObj).not.toEqual(expect.any(Array));
-        expect(usersObj).toEqual(expect.any(Object));
-    });
+		expect(usersObj).not.toEqual(expect.any(Array));
+		expect(usersObj).toEqual(expect.any(Object));
+	});
 
-    it("Debería de obtener un objeto de tipo user", async () => {
-        const userObj = await db.findById(1);
+	it('Debería de obtener un objeto de tipo user', async () => {
+		const userObj = await db.findById(1);
 
-        expect(Object.keys(userObj)).toEqual(
-            expect.arrayContaining(["id", "firstname", "lastname", "email"])
-        );
-    });
+		expect(Object.keys(userObj)).toEqual(
+			expect.arrayContaining([
+				'id',
+				'firstname',
+				'lastname',
+				'email'
+			])
+		);
+	});
 
-    it("Debería de obtener un usuario por id", async () => {
-        const userObj = await db.findById(2);
-        const userValues = Object.values(users[1]);
+	it('Debería de obtener un usuario por id', async () => {
+		const userObj = await db.findById(2);
+		const userValues = Object.values(users[1]);
 
-        expect(Object.values(userObj)).toEqual(
-            expect.arrayContaining(userValues)
-        );
-    });
+		expect(Object.values(userObj)).toEqual(expect.arrayContaining(userValues));
+	});
 });
 
-describe("Servicio create", () => {
-    let newUser = {};
+describe('Servicio create', () => {
+	let newUser = {};
 
-    beforeEach(async () => {
-        await db.clear(); //Limpiar la DB
+	beforeEach(async () => {
+		await db.clear(); //Limpiar la DB
 
-        newUser = {
-            id: 10,
-            firstname: faker.name.firstName(),
-            lastname: faker.name.lastName(),
-            email: faker.internet.email().toLowerCase()
-        };
-    });
+		newUser = {
+			id        : 10,
+			firstname : faker.name.firstName(),
+			lastname  : faker.name.lastName(),
+			email     : faker.internet.email().toLowerCase()
+		};
+	});
 
-    it("Debería de regresar un objeto de tipo user", async () => {
-        const userCreated = await db.create(newUser);
+	it('Debería de regresar un objeto de tipo user', async () => {
+		const userCreated = await db.create(newUser);
 
-        expect(Object.keys(userCreated)).toEqual(
-            expect.arrayContaining(["id", "firstname", "lastname", "email"])
-        );
-    });
+		expect(Object.keys(userCreated)).toEqual(
+			expect.arrayContaining([
+				'id',
+				'firstname',
+				'lastname',
+				'email'
+			])
+		);
+	});
 
-    it("Debería de regresar un objeto de tipo user con los valores que se acaban de agregar", async () => {
-        const userCreated = await db.create(newUser);
+	it('Debería de regresar un objeto de tipo user con los valores que se acaban de agregar', async () => {
+		const userCreated = await db.create(newUser);
+		//Obtener el usuario que se agrego en la DB
+		const userObj = await db.findById(userCreated.id);
 
-        //Obtener el usuario que se agrego en la DB
-        const userObj = await db.findById(userCreated.id);
-
-        expect(Object.values(userCreated)).toEqual(
-            expect.arrayContaining(Object.values(userObj))
-        );
-    });
+		expect(Object.values(userCreated)).toEqual(expect.arrayContaining(Object.values(userObj)));
+	});
 });
 
-describe("Servicio update", () => {
-    let newUser = {};
-    let userCreated = {};
+describe('Servicio update', () => {
+	let newUser = {};
+	let userCreated = {};
 
-    beforeEach(async () => {
-        await db.clear(); //Limpiar la DB
+	beforeEach(async () => {
+		await db.clear(); //Limpiar la DB
 
-        newUser = {
-            id: 12,
-            firstname: faker.name.firstName(),
-            lastname: faker.name.lastName(),
-            email: faker.internet.email().toLowerCase()
-        };
+		newUser = {
+			id        : 12,
+			firstname : faker.name.firstName(),
+			lastname  : faker.name.lastName(),
+			email     : faker.internet.email().toLowerCase()
+		};
 
-        userCreated = await db.create(newUser); //Crear un nuevo usuario
+		userCreated = await db.create(newUser); //Crear un nuevo usuario
+	});
 
-    });
+	it('Debería de regresar un objeto de tipo User al actualizar', async () => {
+		const userObj = await db.update(
+			{
+				firstname : faker.name.firstName(),
+				lastname  : faker.name.lastName(),
+				email     : faker.internet.email().toLowerCase()
+			},
+			12
+		);
+		expect(Object.keys(userObj)).toEqual(
+			expect.arrayContaining([
+				'id',
+				'firstname',
+				'lastname',
+				'email'
+			])
+		);
+	});
 
-    it("Debería de regresar un objeto de tipo User al actualizar", async () => {
-        
-    });
+	it('Debería de regresar un objeto de tipo User con los valores que se acaban de actualizar', async () => {
+		const userUpdate = {
+			firstname : faker.name.firstName(),
+			lastname  : faker.name.lastName(),
+			email     : faker.internet.email().toLowerCase()
+		};
+		const updated = await db.update(userUpdate, 12);
+		expect(Object.values(updated)).toEqual(expect.arrayContaining(Object.values(userUpdate)));
+	});
 
-    it("Debería de regresar un objeto de tipo User con los valores que se acaban de actualizar", async () => {
-        
-    });
-
-    it("Debería de arrojar un error al tratar de actualizar un usuario que no existe en la DB", () => {
-
-    });
+	it('Debería de arrojar un error al tratar de actualizar un usuario que no existe en la DB', () => {
+		expect(async () => await db.update({}, Date.now)).toThrow();
+	});
 });
 
-describe("Servicio delete", () => {
-    let newUser = {};
-    let userCreated = {};
+describe('Servicio delete', () => {
+	let newUser = {};
+	let userCreated = {};
 
-    beforeEach(async () => {
-        await db.clear(); //Limpiar la DB
+	beforeEach(async () => {
+		await db.clear(); //Limpiar la DB
 
-        newUser = {
-            id: 13,
-            firstname: faker.name.firstName(),
-            lastname: faker.name.lastName(),
-            email: faker.internet.email().toLowerCase()
-        };
+		newUser = {
+			id        : 13,
+			firstname : faker.name.firstName(),
+			lastname  : faker.name.lastName(),
+			email     : faker.internet.email().toLowerCase()
+		};
 
-        userCreated = await db.create(newUser); //Crear un nuevo usuario
+		userCreated = await db.create(newUser); //Crear un nuevo usuario
+	});
 
-    });
+	it('Debería de regresar true al eliminar un usuario', async () => {
+		const isUserDeleted = await db.delete(userCreated.id);
 
-    it("Debería de regresar true al eliminar un usuario", async () => {
-        
-    });
+		expect(isUserDeleted).toBe(true);
+	});
 
-    it("Debería de regresar false al tratar de eliminar un usuario con un id que no existe en la DB", async () => {
-        
-    });
+	it('Debería de regresar false al tratar de eliminar un usuario con un id que no existe en la DB', async () => {
+		const isUserDeleted = await db.delete(Date.now());
+		expect(isUserDeleted).toBe(false);
+	});
 });
